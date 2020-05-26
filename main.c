0fac0ee8d837e0e4e7fae020f9539136699df2b0
geferay <30806093+geferay@users.noreply.github.com>
2020-05-26 14:29:22 -0700

    It works!

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 
 /*Global constants*/
+#define DEBUG_LEVEL 0
 #define INPUT_MAX 40
 /*We got to have a version that's +1, for the null terminator.*/
 #define INPUT_LEN 41
@@ -43,7 +44,9 @@ void runStuff(dataStruct * theData, char * memoryPool);
 void firstFit(char *memoryPool, char program, int amount);
 void bestFit(char *memoryPool, char program, int amount);
 void worstFit(char *memoryPool, char program, int amount);
-
+void executionFunction(dataStruct * theData, char * memoryPool, char * scriptFile);
+void stdinRunner(dataStruct * theData, char * memoryPool);
+void fileRunner(dataStruct * theData, char * memoryPool, char * scriptFile);
 
 /*Main function*/
 /*We don't worry about input. It will just be ignored.*/
@@ -55,132 +58,24 @@ int main(){
 	const char *memoryTemplate = "................................................................................";
 	char *memoryPool;
 	memoryPool = (char*)malloc(MEMORY_LEN * sizeof(char));
-	memoryPool[MEMORY_LEN - 1] = '\0';
+	memoryPool[MEMORY_LEN - 1] = '\0'; /*set the 81st character to the null character, because it's the end of a string*/
 	for(int i = 0; memoryTemplate[i]; ++i) memoryPool[i] = memoryTemplate[i];
-	
-	
-	//memoryPool[80] = '\0'; /*set the 81st character (81-1) to the null character, because it's the end of the string*/
 	
 	/*main program loop code*/
 	int notDone = 1; /*We're never done, unless somebody implements an exit command or hits Ctr-C*/
 	dataStruct * theData = (dataStruct*)malloc(sizeof(dataStruct));
 	while(notDone){
-		/*Get ready for input. We need to do a command prompt with switch() statement.*/
-		theData->inputMax   = INPUT_MAX;
-		theData->charInput  =  0;        /*getline() tells us the input size*/
-		theData->input[INPUT_LEN];   /*Way too much space. The likely max needed is 9.*/
-		theData->command    = '\0';
-		theData->program    = '\0';
-		theData->amount     =    0;
-		theData->algorithm  = '\0';
-		theData->parsed     = NULL;
-		theData->curr       = 0;         /*current position in input string*/
-		theData->errorFound = 0;
+			theData->inputMax   = INPUT_MAX;
+			theData->charInput  =  0;        /*getline() tells us the input size*/
+			theData->command    = '\0';
+			theData->program    = '\0';
+			theData->amount     =    0;
+			theData->algorithm  = '\0';
+			theData->parsed     = NULL;
+			theData->curr       = 0;         /*current position in input string*/
+			theData->errorFound = 0;
 		
-		/*getInput*/
-		printf("Give a command: ");
-		char *in = theData->input; /*ugly hack to get getline() to be happy*/
-		theData->charInput = getline(&in, &theData->inputMax, stdin);
-		if(theData->charInput <= 1) continue; /*nothing to do here, just repeat the loop*/
-		/*remove trailing newline*/
-		if(theData->input[theData->charInput - 1] == 10) theData->input[theData->charInput - 1] = '\0';
-		
-		
-		/*skip leading spaces*/
-		while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
-		/*assign command*/
-		theData->command = theData->input[theData->curr++];
-		
-		/*parsing section===============================================================================*/
-		if(theData->command == 'R' || theData->command == 'r'){
-			theData->command = 'R';
-			/*error on extra non-space characters*/
-			if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
-			if(!theData->errorFound){
-				/*skip space characters and tabs*/
-				while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
-				/*Get the file name*/
-				char *fileName;
-				int fileCurr = 0;
-				fileName = (char*)malloc((INPUT_LEN - 2)*sizeof(char));
-				while(theData->input[theData->curr]) fileName[fileCurr++] = theData->input[theData->curr++];
-				fileName[fileCurr] = '\0';
-				printf("The file name received is \"%s\"\n", fileName);
-				/*read the commands from a file*/
-			}
-		}
-		else if(theData->command == 'S' || theData->command == 's'){
-			theData->command = 'S';
-		}
-		else if(theData->command == 'C' || theData->command == 'c'){
-			theData->command = 'C';
-			/*compact the memory*/
-		}
-		else if(theData->command == 'F' || theData->command == 'f'){
-			theData->command = 'F';
-			/*error on extra non-space characters*/
-			if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
-			if(!theData->errorFound){
-				/*skip spaces and tabs*/
-				while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
-				/*assign program*/
-				theData->program = theData->input[theData->curr];
-				/*make uppercase*/
-				if(theData->program > 90) theData->program -= 32;
-			}
-		}
-		else if(theData->command == 'A' || theData->command == 'a'){
-			theData->command = 'A';
-			//printf("Running memory allocator %s\n", (theData->input + theData->curr));
-			/*error on extra non-space characters*/
-			if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
-			if(!theData->errorFound){
-			
-				/*skip spaces and tabs*/
-				while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
-				/*assign program*/
-				theData->program = theData->input[theData->curr++];
-				/*make uppercase*/
-				if(theData->program > 90) theData->program -= 32;
-				
-				
-				/*error on extra non-space characters*/
-				if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
-				if(!theData->errorFound){
-					/*skip spaces and tabs*/
-					while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
-					
-					//printf("Running memory allocator %s\nProgram is %c\n", (theData->input + theData->curr), theData->program);
-					/*get integer from text, for space size to allocate*/
-					int num = 0;
-					int mult = 0;
-					while((theData->input[theData->curr] >= '0') && (theData->input[theData->curr] <= '9')){
-						//printf("num is %d, mult is %d, input[%d] is %c\n", num, mult, theData->curr, theData->input[theData->curr]);
-						if(mult) num *= 10;
-						num += theData->input[theData->curr++] - 48;
-						mult = 1;
-					}
-					//printf("Number is %d\n", num);
-					theData->amount = num;
-					/*error on extra non-space characters*/
-					if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
-					if(!theData->errorFound){
-					
-						/*skip spaces and tabs*/
-						while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
-						/*assign algorithm*/
-						theData->algorithm = theData->input[theData->curr++];
-						/*make uppercase*/
-						if(theData->algorithm > 90) theData->algorithm -= 32;
-					}
-				}
-			}
-		}
-		else{
-			theData->errorFound = 1; /*There must have been an error.*/
-		}
-		
-		runStuff(theData, memoryPool);
+		stdinRunner(theData, memoryPool);
 	}
 	
 	if(memoryPool){
@@ -195,9 +90,167 @@ int main(){
 	return 0; /*Every day's a good day! :-) */
 }
 
+
+/*function to run commands from stdin*/
+void stdinRunner(dataStruct * theData, char * memoryPool){
+		printf("Give a command: ");
+		char *inp = theData->input; /*ugly hack to get getline() to be happy*/
+		theData->charInput = getline(&inp, &theData->inputMax, stdin);
+		if(theData->charInput <= 1) return; /*nothing to do here, just repeat the loop*/
+		executionFunction(theData, memoryPool, theData->input);
+}
+
+
+/*function to run commands from file*/
+void fileRunner(dataStruct * theData, char * memoryPool, char * scriptFile){
+	FILE *in;
+	if(DEBUG_LEVEL >= 1) printf("Loading file: %s\n", scriptFile);
+	in = fopen(scriptFile,"r");
+	if(!in){
+		printf("We're sorry, but the file failed to load. :-(\nDon't give up!\n");
+		return; /*get out of here*/
+	}
+	
+	while(fgets(theData->input,INPUT_LEN,in) != NULL){
+		/*reset our variables*/
+			theData->charInput  =  0;        /*we will store the input length*/
+			theData->command    = '\0';
+			theData->program    = '\0';
+			theData->amount     =    0;
+			theData->algorithm  = '\0';
+			theData->parsed     = NULL;
+			theData->curr       = 0;         /*current position in input string*/
+			theData->errorFound = 0;
+			
+		for(int i = 0; theData->input[i]; ++i) ++theData->charInput;
+		if(theData->charInput <= 1) continue; /*nothing to do here, just repeat the loop*/
+		if(DEBUG_LEVEL >= 2) printf("read line: %s\n", theData->input);
+		
+		
+		if(DEBUG_LEVEL >= 2) printf("ENTERING executionFunction(). theData->input = %s\n", theData->input);
+		executionFunction(theData, memoryPool, theData->input);
+	}
+	
+	fclose(in);
+}
+
+
+/*function to execute commands*/
+void executionFunction(dataStruct * theData, char * memoryPool, char * command){
+			/*remove trailing newline* - It may be: LF, CR, CR+LF, or any combination of 3 LF/CR characters.*/
+			if((theData->input[theData->charInput - 1] == 10) || (theData->input[theData->charInput - 1] == 13)) theData->input[--theData->charInput] = '\0';
+			if((theData->input[theData->charInput - 1] == 10) || (theData->input[theData->charInput - 1] == 13)) theData->input[--theData->charInput] = '\0';
+			if((theData->input[theData->charInput - 1] == 10) || (theData->input[theData->charInput - 1] == 13)) theData->input[--theData->charInput] = '\0';
+			
+			if(DEBUG_LEVEL >= 2) printf("End-lines removed. theData->input = %s\n", theData->input);
+			
+			/*skip leading spaces*/
+			while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
+			if(DEBUG_LEVEL >= 1) printf("File processing: theData->input = %s\n", theData->input);
+			/*assign command*/
+			theData->command = theData->input[theData->curr++];
+			
+			/*parsing section===============================================================================*/
+			if(theData->command == 'R' || theData->command == 'r'){
+				theData->command = 'R';
+				/*error on extra non-space characters*/
+				if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
+				if(!theData->errorFound){
+					/*skip space characters and tabs*/
+					while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
+					/*Get the file name*/
+					char *fileName;
+					int fileCurr = 0;
+					fileName = (char*)malloc((INPUT_LEN - 2)*sizeof(char));
+					while(theData->input[theData->curr]) fileName[fileCurr++] = theData->input[theData->curr++];
+					fileName[fileCurr] = '\0';
+					if(DEBUG_LEVEL >= 2) printf("The file name received is \"%s\"\n", fileName);
+					/*read the commands from a file*/
+					theData->parsed = fileName;
+				}
+			}
+			else if(theData->command == 'S' || theData->command == 's'){
+				theData->command = 'S';
+			}
+			else if(theData->command == 'C' || theData->command == 'c'){
+				theData->command = 'C';
+				/*compact the memory*/
+			}
+			else if(theData->command == 'F' || theData->command == 'f'){
+				theData->command = 'F';
+				/*error on extra non-space characters*/
+				if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
+				if(!theData->errorFound){
+					/*skip spaces and tabs*/
+					while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
+					/*assign program*/
+					theData->program = theData->input[theData->curr];
+					/*make uppercase*/
+					if(theData->program > 90) theData->program -= 32;
+				}
+			}
+			else if(theData->command == 'A' || theData->command == 'a'){
+				theData->command = 'A';
+				if(DEBUG_LEVEL >= 2) printf("Running memory allocator %s\n", (theData->input + theData->curr));
+				/*error on extra non-space characters*/
+				if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
+				if(!theData->errorFound){
+					/*skip spaces and tabs*/
+					while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
+					
+					/*assign program*/
+					theData->program = theData->input[theData->curr++];
+					
+					/*make uppercase*/
+					if(theData->program > 90) theData->program -= 32;
+					
+					/*error on extra non-space characters*/
+					if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
+					if(!theData->errorFound){
+						/*skip spaces and tabs*/
+						while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
+						
+						if(DEBUG_LEVEL >= 1) printf("Running memory allocator %s\nProgram is %c\n", (theData->input + theData->curr), theData->program);
+						/*get integer from text, for space size to allocate*/
+						int num = 0;
+						int mult = 0;
+						while((theData->input[theData->curr] >= '0') && (theData->input[theData->curr] <= '9')){
+							if(DEBUG_LEVEL >= 2) printf("num is %d, mult is %d, input[%d] is %c\n", num, mult, theData->curr, theData->input[theData->curr]);
+							if(mult) num *= 10;
+							num += theData->input[theData->curr++] - 48;
+							mult = 1;
+						}
+						if(DEBUG_LEVEL >= 2) printf("Number is %d\n", num);
+						theData->amount = num;
+						/*error on extra non-space characters*/
+						if(theData->input[theData->curr] != ' ' && theData->input[theData->curr] != 9) theData->errorFound = 1;
+						if(!theData->errorFound){
+						
+							/*skip spaces and tabs*/
+							while(theData->input[theData->curr] == ' ' || theData->input[theData->curr] == 9) ++theData->curr;
+							/*assign algorithm*/
+							theData->algorithm = theData->input[theData->curr++];
+							/*make uppercase*/
+							if(theData->algorithm > 90) theData->algorithm -= 32;
+						}
+					}
+				}
+			}
+			else{
+				theData->errorFound = 1; /*There must have been an error.*/
+			}
+			
+			runStuff(theData, memoryPool);
+			
+			/*return nothing;*/
+	
+}
+
+
+/*function to runStuff*/
 void runStuff(dataStruct * theData, char * memoryPool){
 	
-		printf("Command is: %c\n", theData->command);
+		if(DEBUG_LEVEL >= 1) printf("Command is: %c\n", theData->command);
 		
 		if(theData->errorFound == 1){
 			/*show error*/
@@ -212,6 +265,7 @@ void runStuff(dataStruct * theData, char * memoryPool){
 				case 'R':{
 					/*setup reading from the file*/
 					/*run commands from file*/
+					fileRunner(theData, memoryPool, theData->parsed);
 				} break;
 				case 'S':{
 					/*show the state of the memory pool*/
@@ -226,7 +280,7 @@ void runStuff(dataStruct * theData, char * memoryPool){
 					freeMemory((char*)memoryPool, theData->program);
 				} break;
 				case 'A':{
-					//printf("Running memory allocator %c to allocate %d bytes to program %c.\n", theData->algorithm, theData->amount, theData->program);
+					if(DEBUG_LEVEL >= 1) printf("Running memory allocator %c to allocate %d bytes to program %c.\n", theData->algorithm, theData->amount, theData->program);
 					switch(theData->algorithm){
 						case 'F':{
 							firstFit(memoryPool, theData->program, theData->amount);
@@ -251,10 +305,12 @@ void runStuff(dataStruct * theData, char * memoryPool){
 	/*return nothing;*/
 }
 
+
+/*function to allocate memory with firstFit algorithm*/
 void firstFit(char *memoryPool, char program, int amount){
 	int location = 0;
 	for(int i = 0; i < MEMORY_SIZE && (amount - (i - location)); ++i){
-		//printf("We're here.\n"); fflush(stdout);
+		if(DEBUG_LEVEL >= 2) printf("We're in the firstFit() for() loop.\n"); fflush(stdout);
 		if(memoryPool[i] != '.') location = i + 1; /*location not empty*/
 	}
 	if(MEMORY_SIZE >= (location + amount)){
@@ -263,12 +319,13 @@ void firstFit(char *memoryPool, char program, int amount){
 		}
 	}
 	else{
-		printf("I'm sorry, Dave. I can't allocate enough memory for that.\n");
+		printf("I'm sorry, Dave. I can't allocate enough memory for that. %d bytes for program %c is more than I can remember.\n", amount, program);
 	}
 	/*return nothing;*/
 }
 
 
+/*function to allocate memory with bestFit algorithm*/
 void bestFit(char *memoryPool, char program, int amount){
 	int beg = 0;
 	int end = 90;
@@ -277,7 +334,7 @@ void bestFit(char *memoryPool, char program, int amount){
 	
 	int location = 0;
 	for(int i = 0; i < MEMORY_SIZE; ++i){
-		//printf("We're here.\n"); fflush(stdout);
+		if(DEBUG_LEVEL >= 2) printf("We're in the bestFit() for() loop.\n"); fflush(stdout);
 		if(memoryPool[i] != '.'){
 			if((testEnd - testBeg) >= amount && (testEnd - testBeg) < (end - beg)){
 				beg = testBeg;
@@ -300,12 +357,13 @@ void bestFit(char *memoryPool, char program, int amount){
 		}
 	}
 	else{
-		printf("I'm sorry, Dave. I can't allocate enough memory for that.\n");
+		printf("I'm sorry, Dave. I can't allocate enough memory for that. %d bytes for program %c is more than I can remember.\n", amount, program);
 	}
 	/*return nothing;*/
 }
 
 
+/*function to allocate memory with worstFit algorithm*/
 void worstFit(char *memoryPool, char program, int amount){
 	int beg = 0;
 	int end = 0;
@@ -314,7 +372,7 @@ void worstFit(char *memoryPool, char program, int amount){
 	
 	int location = 0;
 	for(int i = 0; i < MEMORY_SIZE; ++i){
-		//printf("We're here.\n"); fflush(stdout);
+		if(DEBUG_LEVEL >= 2) printf("We're in the worstFit() for() loop.\n"); fflush(stdout);
 		if(memoryPool[i] != '.'){
 			if((testEnd - testBeg) >= amount && (testEnd - testBeg) > (end - beg)){
 				beg = testBeg;
@@ -337,14 +395,15 @@ void worstFit(char *memoryPool, char program, int amount){
 		}
 	}
 	else{
-		printf("I'm sorry, Dave. I can't allocate enough memory for that.\n");
+		printf("I'm sorry, Dave. I can't allocate enough memory for that. %d bytes for program %c is more than I can remember.\n", amount, program);
 	}
 	/*return nothing;*/
 }
 
+
 /*function to compact the memory*/
 void compactMemory(char *memoryPool){
-	printf("compacting memory\n");
+	if(DEBUG_LEVEL >= 2) printf("compacting memory\n");
 	/*put code here to compact the memory*/
 	int availabeSpace = 0;
 	for(int i = 0; memoryPool[i]; ++i){
@@ -357,9 +416,10 @@ void compactMemory(char *memoryPool){
 	/*return nothing;*/
 }
 
+
 /*function to free the memory for a program*/
 void freeMemory(char *memoryPool, char program){
-	printf("Freeing memory for program %c\n", program);
+	if(DEBUG_LEVEL >= 2) printf("Freeing memory for program %c\n", program);
 	for(int i = 0; memoryPool[i]; ++i){
 		if(memoryPool[i] == program) memoryPool[i] = '.';
 	}
